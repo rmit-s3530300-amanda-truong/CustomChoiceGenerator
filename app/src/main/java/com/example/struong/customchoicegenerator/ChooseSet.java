e11cfbd024dc0c2d2b4e776b49a87727b7251d06
Amanda Truong <s3530300@student.rmit.edu.au>
2017-02-16 17:10:51 +1100

    in choose set : doesnt allow input of less than 1, still need to check for empty input

--- app/src/main/java/com/example/struong/customchoicegenerator/ChooseSet.java
+++ app/src/main/java/com/example/struong/customchoicegenerator/ChooseSet.java
@@ -3,6 +3,7 @@ package com.example.struong.customchoicegenerator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,23 +16,34 @@ public class ChooseSet extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_set);
 
-        final int[] setAnswer = new int[1];
         final Button bOkSet = (Button) findViewById(R.id.bOKSet);
-        final EditText etSetAnswer = (EditText) findViewById(R.id.etSetAnswer);
+
+            bOkSet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EditText etSetAnswer = (EditText) findViewById(R.id.etSetAnswer);
+                    String setStr = etSetAnswer.getText().toString();
+                    int setAnswer;
+                    setAnswer = Integer.parseInt(setStr);
+
+                    if(setAnswer == 0){
+                        etSetAnswer.setError("Value must be at least 1");
+                    }
+
+                    //else if(){
+                    // check if value is empty
+                    //}
+                    else{
+                        Intent chooseOptIntent = new Intent(ChooseSet.this, ChooseOptions.class);
+                        chooseOptIntent.putExtra("setAnswer", setAnswer);
+                        ChooseSet.this.startActivity(chooseOptIntent);
+                    }
 
 
-        bOkSet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String setStr = etSetAnswer.getText().toString();
-                setAnswer[0] = Integer.parseInt(setStr);
-                Intent chooseOptIntent = new Intent(ChooseSet.this, ChooseOptions.class);
-                chooseOptIntent.putExtra("setAnswer",setAnswer);
-                ChooseSet.this.startActivity(chooseOptIntent);
-            }
-        });
+                }
+
+            });
+
+
     }
-
-
-
 }
